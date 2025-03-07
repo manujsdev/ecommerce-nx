51cdcf908676a25af7d83011704fac322603ed2c
Manu <manujsdev@gmail.com>
2025-03-06 21:37:02 -0500

    added product app

---
+++ apps/product/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { DatasourceModule } from '@ecommerce-nx/datasource';
+
+@Module({
+  imports: [
+    DatasourceModule.forRootAsync({
+      useFactory: () => {
+        return {
+          type: 'postgres',
+          // host: configuration().postgres_host,
+          // port: configuration().postgres_port,
+          // username: configuration().postgres_user,
+          // password: configuration().postgres_password,
+          // database: configuration().postgres_database,
+          logging: false,
+          poolSize: 8,
+          applicationName: 'product-catalog',
+        };
+      },
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
